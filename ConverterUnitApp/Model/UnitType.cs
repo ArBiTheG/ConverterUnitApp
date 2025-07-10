84fda615e5dcead369ccd10c10b950cab5c894cd
ArBiTheG <56531440+ArBiTheG@users.noreply.github.com>
2025-07-10 21:32:22 +0300

    Добавлены новые величины

--- ConverterUnitApp/Model/UnitType.cs
+++ ConverterUnitApp/Model/UnitType.cs
@@ -8,13 +8,15 @@ namespace ConverterUnitApp.Model
 {
     internal class UnitType
     {
-        public UnitType(string name)
+        public UnitType(string name, string description)
         {
             Name = name;
+            Description = description;
             Units = new List<Unit>();
         }
 
         public string Name { get; set; }
+        public string Description { get; set; }
         public List<Unit> Units { get; set; }
     }
 }
--- ConverterUnitApp/Services/MainService.cs
+++ ConverterUnitApp/Services/MainService.cs
@@ -12,7 +12,9 @@ namespace ConverterUnitApp.Services
         {
             _unitTypes = new List<UnitType>();
 
-            InitBaseConversion();
+            InitLengthConversion();
+            InitMassConversion();
+            InitTempConversion();
         }
 
         public List<UnitType> UnitTypes { get { return _unitTypes; } }
@@ -22,15 +24,33 @@ namespace ConverterUnitApp.Services
             return (value * from.Factor) / to.Factor;
         }
 
-        private void InitBaseConversion()
+        private void InitLengthConversion()
         {
-            var length = CreateType("Длина");
+            var length = CreateType("Длина", "Физическая величина, характеризующая протяжённость объекта или расстояние между двумя точками в пространстве.");
             var mm = CreateUnit(length, "Милиметр", "мм", 0.001m);
             var cm = CreateUnit(length, "Сантиметр", "см", 0.01m);
             var inch = CreateUnit(length, "Дюйм", "\"", 0.0254m);
             var ft = CreateUnit(length, "Фут", "фт", 0.3048m);
-            var m  = CreateUnit(length, "Метр", "м", 1);
+            var m = CreateUnit(length, "Метр", "м", 1);
             var km = CreateUnit(length, "Километр", "км", 1000);
+        }
+        private void InitMassConversion()
+        {
+            var mass = CreateType("Масса", "Физическая величина, которая показывает, какое количество вещества содержится в теле");
+            var mg = CreateUnit(mass, "Миллиграмм", "мг", 0.000001m);
+            var g = CreateUnit(mass, "Грамм", "г", 0.001m);
+            var u = CreateUnit(mass, "Унции", "фт", 0.02835m);
+            var f = CreateUnit(mass, "Фунты", "фт", 0.4095m);
+            var kg = CreateUnit(mass, "Килограмм", "кг", 1);
+            var t = CreateUnit(mass, "Тонна", "т", 1000);
+        }
+        private void InitTempConversion()
+        {
+            var temp = CreateType("Объём", "Физическая величина, которая показывает, какое пространство (какую часть пространства) занимает тело или вещество.");
+            var ml = CreateUnit(temp, "Миллилитр", "л", 0.000001m);
+            var l = CreateUnit(temp, "Литр", "л", 0.001m);
+            var g = CreateUnit(temp, "Галлон", "гл", 0.00454609m);
+            var m = CreateUnit(temp, "Кубический метр", "m3", 1);
         }
 
         private Unit CreateUnit(UnitType type, string name, string symbol = "", decimal factor = 1)
@@ -39,9 +59,9 @@ namespace ConverterUnitApp.Services
             type.Units.Add(unit);
             return unit;
         }
-        private UnitType CreateType(string name)
+        private UnitType CreateType(string name, string description)
         {
-            UnitType type = new UnitType(name);
+            UnitType type = new UnitType(name, description);
 
             _unitTypes.Add(type);
             return type;
